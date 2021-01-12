4ac65e94db30faeca74d3c41bf9eab3cff86a2d5
Joao Gilberto Magalhaes <joao@byjg.com.br>
2021-01-11 20:39:45 -0600

    Add example

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # terraform-localstack
+
 Apply a terraform plan on Localstack
+
+## Instructions
+
+
+Change the `docker-compose.yaml` to add the desired services. 
+
+The new localstack exposes the port 4566 for all services. 
+
+```
+docker-compose up -d 
+```
+
+## Testing if the connection is working:
+
+```
+export AWS_ACCESS_KEY_ID=foo
+export AWS_SECRET_ACCESS_KEY=bar
+export AWS_REGION=us-east-1
+
+aws --endpoint-url=http://localhost:4566 s3 mb s3://local-bucket
+aws --endpoint-url=http://localhost:4566 s3 ls
+```
+
+## Run the terraform plan/apply
+
+To run the example provided just do:
+
+```
+terraform init
+terraform plan
+terraform apply
+```
+
+
+## References
+
+* https://registry.terraform.io/providers/hashicorp/aws/latest/docs/guides/custom-service-endpoints#localstack
+* https://github.com/localstack/localstack
+
+
+
+
---
+++ terraform.tf
@@ -0,0 +1,67 @@
+# local
+
+terraform {
+  backend "local" {}
+}
+
+provider "aws" {
+  access_key                  = "mock_access_key"
+  region                      = "us-east-1"
+  s3_force_path_style         = true
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3     = "http://0.0.0.0:4566"
+    lambda = "http://0.0.0.0:4566"
+    iam    = "http://0.0.0.0:4566"
+  }
+}
+
+
+# Example:
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "stub.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "exports.test"
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+  source_code_hash = filebase64sha256("stub.zip")
+
+  runtime = "nodejs12.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+
+
